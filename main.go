46f733b0273726900543dd13e03ff680a513385d
Luke Massa <lmassa@tripadvisor.com>
2022-01-23 04:30:38 -0500

    Fix formatting

--- main.go
+++ main.go
@@ -12,12 +12,12 @@ func main() {
 	log.SetFormatter(customFormatter)
 	customFormatter.FullTimestamp = true
 
-	//player := players.PreferClearingPlayer{}
-	//	player := players.UpperLeftPlayer{}
+	// player := players.PreferClearingPlayer{}
+	// player := players.UpperLeftPlayer{}
 	// player := players.TextBasedPlayer{}
 	player := players.TUIPlayer{}
-	//game.RunTrials(&player, 100_00)
-	//game.RunTrialsByTime(player, 10*time.Second)
+	// game.RunTrials(&player, 100_00)
+	// game.RunTrialsByTime(player, 10*time.Second)
 	game.RunInteractive(player)
 
 }
